58073f3fb2af6244b4fa76b10467701c9f047810
Logan Kania <ljkania5@gmail.com>
2026-04-29 15:51:39 -0400

    Initial commit: FastAPI hello world

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return "<h1>Hello, world!</h1><p>My portfolio site is alive.</p>"
